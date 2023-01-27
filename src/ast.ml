106e270fe69079e4ad5deed253c217e9db85e358
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2023-01-27 12:36:53 +0000

    AST

--- src/ast.ml
+++ src/ast.ml
@@ -11,8 +11,9 @@ type expr =
   (* Apply to the function this expr list *)
   | Apply of string * expr list
 and stmt =
+  | Return of expr
   (* Define variables with name, optional typ and a value *)
   | Let of string * typ option * expr
   (* Name | Params with name type | Return type | Statements for the body function *)
-  | Func of string * (string * typ) list * typ * stmt list
+  | Func of string * (string * typ) list * typ option * stmt list
 
